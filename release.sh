8084d2c77ec7792e518425287afb136b4436fcbc
renyunkang <rykren1998@gmail.com>
2023-09-06 15:15:54 +0800

    replace the cni image
    Signed-off-by: renyunkang <rykren1998@gmail.com>

---
+++ release.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+REPO=${REPO:-rykren}
+TAG=${TAG:-latest}
+IMAGE=${IMAGE:-cni}
+PUSH=${PUSH:-false}
+arch=(amd64 arm64 armv7 ppc64le s390x)
+
+# build docker IMAGE
+for i in "${arch[@]}"
+do
+   make -C cni-plugin image DEV_REGISTRIES=$REPO ARCH=$i
+   if [ "$TAG" != "latest" ]; then
+      make retag-build-images-with-registries DEV_REGISTRIES=$REPO BUILD_IMAGES=$IMAGE VALIDARCHES=$i IMAGETAG=$TAG
+   fi
+done
+
+# push image manifest
+if [ "$PUSH" = true ]; then
+   manifest=""
+   for i in "${arch[@]}"
+   do
+      # make cd-common DEV_REGISTRIES=$REPO BUILD_IMAGES=$IMAGE BRANCH_NAME=$TAG IMAGETAG=$TAG VALIDARCHES=$i CONFIRM=1
+      # make push-images-to-registries DEV_REGISTRIES=$REPO BUILD_IMAGES=$IMAGE IMAGETAG=$TAG VALIDARCHES=$i CONFIRM=1
+      # make push-manifests DEV_REGISTRIES=$REPO BUILD_IMAGES=$IMAGE IMAGETAG=$TAG VALIDARCHES=$i CONFIRM=1
+      docker push $REPO/$IMAGE:$TAG-$i
+      manifest+=$REPO/$IMAGE:$TAG-$i
+      manifest+=" "
+   done
+
+   docker manifest create $REPO/$IMAGE:$TAG $manifest --amend
+   docker manifest push $REPO/$IMAGE:$TAG
+fi
+
